7071bd461080210a7240abe0f89bab99dae49382
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-09-27 23:57:11 -0400

    Updates

---
+++ precache-manifest.d81e4da5c4523f8b6533587ed9e3a21f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4899d64d381f81e49eeab5a9de93b05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11f20012db11636643b7",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "d182a223da3d6e739588",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "11f20012db11636643b7",
+    "url": "/static/js/2.b785c9cb.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.b785c9cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d182a223da3d6e739588",
+    "url": "/static/js/main.be7832db.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
