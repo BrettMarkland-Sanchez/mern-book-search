972b27c1046a5b8c71c1e5471d0058d5170d28db
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-10-07 20:54:41 -0400

    adjusted resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,9 +4,6 @@ const { User } = require('../models');
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find();
-    },
     user: async (_, args) => {
       return User.findOne({ _id: args.id });
     },
