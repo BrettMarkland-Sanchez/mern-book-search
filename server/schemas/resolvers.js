ff52595ea72d602c76db656af3e52dd3cc0cf7ed
Brett Markland-Sanchez <Brett.MarklandSanchez@gmail.com>
2021-10-07 20:40:36 -0400

    attempt boilerplate reset

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,54 +1,47 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
 const { signToken } = require('../utils/auth');
+const { User } = require('../models');
 
 const resolvers = {
   Query: {
-    me: async (parent, args, context) => {
+    users: async () => {
+      return User.find();
+    },
+    user: async (_, args) => {
+      return User.findOne({ _id: args.id });
+    },
+    me: async (_, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-        return userData
-      } else throw new AuthenticationError('You need to be logged in!');
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
   },
 
   Mutation: {
-    addUser: async (parent, { username, email, password }) => {
+    addUser: async (_, { username, email, password }) => {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
     },
-    login: async (parent, { email, password }) => {
+    login: async (_, { email, password }) => {
       const user = await User.findOne({ email });
-        if (!user) { throw new AuthenticationError('No profile with this email found!') };
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
       const correctPw = await user.isCorrectPassword(password);
-        if (!correctPw) { throw new AuthenticationError('Incorrect password!') };
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
       const token = signToken(user);
+
       return { token, user };
-    },
- 
-    saveBook: async (parent, { bookId, authors,  description, image, link, title }, context) => { 
-      if (context.user) {
-        const updatedUser = await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { savedBooks: { bookId, authors,  description, image, link, title } } },
-          { new: true, runValidators: true }
-        );
-        return updatedUser;
-      } else throw new AuthenticationError('You need to be logged in!');
-    },
- 
-    removeBook: async (parent, { bookId }, context) => {
-      if (context.user) {
-        const updatedUser = await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { savedBooks: { bookId } } },
-          { new: true }
-        );
-        return updatedUser;
-      } else throw new AuthenticationError('You need to be logged in!');
-    },
-  },
+    }
+  }
 };
 
 module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,35 +1,37 @@
-// Server imports
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-
-// Instantiate express app and assign port
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Configure app
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.get('*', (req, res) => { res.sendFile(path.join(__dirname, '../client/build/index.html')) });
-
-// Import, instantiate Apollo server and params
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-const server = new ApolloServer({ typeDefs, resolvers, context: authMiddleware });
 
-// Connect Apollo server with Express app
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
 server.applyMiddleware({ app });
 
-// If in production, serve client/build as static assets
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// App listener
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 db.once('open', () => {
-  app.listen( PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  })
+  });
 });
